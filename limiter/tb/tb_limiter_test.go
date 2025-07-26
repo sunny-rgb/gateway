3c27eec29fa91abf1d6abb6a05301c4826cee7aa
Mr.L <2771997672@qq.com>
2025-07-27 00:29:39 +0800

    init commit

---
+++ limiter/tb/tb_limiter_test.go
@@ -0,0 +1,32 @@
+package tb
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestTBLimiter_Allow(t *testing.T) {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "",
+		Password: "",
+	})
+	tbLimiter, err := NewTBLimiter(context.TODO(), redisClient)
+	if err != nil {
+		panic(err)
+	}
+
+	tbLimit := &TBLimit{Rate: 100, Burst: 100, Expire: 10}
+	result, err := tbLimiter.Allow(context.TODO(), "/foo", tbLimit)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if result.Allowed > 0 {
+		fmt.Printf("limit allowed: %+v, remining: %+v\n", result.Allowed, result.Remaining)
+	} else {
+		fmt.Printf("limit not allowed: %+v, remining: %+v\n", result.Allowed, result.Remaining)
+	}
+
+}
